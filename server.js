a5a932e7253331c0dda4e3cd6cb5e79d9bcf7a11
chessrehab <martin.svec@gmail.com>
2019-02-24 13:43:04 +0100

    heroku setup, startscript and port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express= require('express');
 const hbs= require('hbs');
 const fs=require('fs');
+const port=process.env.PORT || 3000;
+
 
 
 var app= express();
@@ -22,7 +24,7 @@ app.use((req,res,next)=>{
 
   next();
 });
-// 
+//
 // app.use((req,res,next)=>{
 //   res.render('maintenance.hbs');
 //   next();
@@ -55,6 +57,6 @@ app.get('/about', (req,res)=>{
   });
   //res.send('About page');
 })
-app.listen(3000,()=> {
-  console.log('server is up on port 3000');
+app.listen(port,()=> {
+  console.log(`server is up on port ${port}`);
 });
